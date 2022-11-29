9ff271fa85a13ec88ee788215c48d378c57ad4e4
Masaharu Kobayashi <masakoby@gmail.com>
2022-11-30 02:27:03 +0900

    Update detect_valid_area_in_hologic_slide.cpp

--- src/detect_valid_area_in_hologic_slide.cpp
+++ src/detect_valid_area_in_hologic_slide.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include "kb_openslide.h"
+#include "kb_xml_io.h"
+#include "kb_xml_ndpa.h"
+#include "kb_searchfiles.h"
+#include "kb_split_string.h"
+#include "kb_cv_string.h"
+#include "kb_csv.h"
+
+
+int main(int argc, char* argv[])
+{
+
+  return 0;
+}
 
